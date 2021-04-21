9fe0cf5337ec5adf5307db11bda4186dab3c9e5a
Timon Grassl <grassl.timon@gmail.com>
2021-04-22 00:48:45 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+
+/*
+Aufgaben:
+1. Erstelle ein Eingabefeld und einen Button um ein neues Todo hinzuzufügen
+2. Implementiere die Logik für das Hinzufügen, wenn der Button gedrückt wird
+3. Speichere die Todos in einem Array und zeige die Inhalte im 'todo-list' div an
+4. Füge eine Möglichkeit zum Löschen von Todos hinzu
+*/
